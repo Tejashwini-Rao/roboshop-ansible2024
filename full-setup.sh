7960e46b58029063a48f3fa81e56ff0c4bdd726e
Tejashwini Rao <polepalle.07@gmail.com>
2024-06-15 15:23:38 +0530

    Pushing all components-shell

--- full-setup.sh
+++ full-setup.sh
@@ -1,5 +1,5 @@
 for COMPONENT in frontend cart catalogue user shipping payment redis mongodb mysql rabbitmq ; do
 
-  ansible-playbook -i ${COMPONENT}-dev.devopst77.online, roboshop.yml -e ansible_user=root -e ansible_password=DevOps321 -e APP_COMPONENT_ROLE=${component} -e ENV=dev
+  ansible-playbook -i ${COMPONENT}-dev.devopst77.online, roboshop.yml -e ansible_user=root -e ansible_password=DevOps321 -e APP_COMPONENT_ROLE=${COMPONENT} -e ENV=dev
 
 done
